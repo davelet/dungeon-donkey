465b15bab20fe19b6099222336ef2a61870a7ff8
Sheldon <Sheldon.Wei.-ND@disney.com>
2024-07-16 22:59:06 +0800

    draw flow

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,42 @@
-use bracket_lib::prelude::{BError, BTerm, BTermBuilder, main_loop};
-use bracket_lib::terminal::GameState;
+mod map;
 
-const SCREEN_WIDTH: i32 = 80;
-const SCREEN_HEIGHT: i32 = 50;
-const FRAME_DURATION: f32 = 75.0;
+use bracket_lib::terminal::GameState;
+use prelude::*;
+
+mod prelude {
+    pub(crate) use bracket_lib::prelude::{BError, BTerm, BTermBuilder, main_loop};
+
+    pub(crate) const SCREEN_WIDTH: i32 = 80;
+    pub(crate) const SCREEN_HEIGHT: i32 = 50;
+
+    pub(crate) use crate::map::*;
+}
+
 
 fn main() -> BError {
-    let c = BTermBuilder::simple80x50().with_title("Dungeon Donkey").build()?;
+    let c = BTermBuilder::simple80x50()
+        .with_fps_cap(30f32)
+        .with_title("Dungeon Donkey").build()?;
     main_loop(c, State::new())
 }
 
-struct State {}
+struct State {
+    map: Map,
+}
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
-        ctx.print(0, 0, "helll");
+        self.map.render(ctx)
     }
 }
 
 
 impl State {
     fn new() -> Self {
-        Self {}
+        Self {
+            map: Map::new()
+        }
     }
 }
 
